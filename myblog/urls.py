fcec31dbb937991a05e4c44c44fe2d8a7801185c
ZXY0806 <402223302@qq.com>
2019-06-05 23:57:53 +0800

    edit views

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
+# app_name = 'myblog'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('register/', views.register, name='register'),
+    path('register/', views.Register.as_view(), name='register'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
 
--- myblog/views.py
+++ myblog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from django.views.generic import View
+from .models import User
 # Create your views here.
 
 
@@ -8,9 +9,16 @@ def index(request):
     return HttpResponse('HelloWorld')
 
 
-def register(request):
-    pass
-    return render(request, 'blog/register.html')
+class Register(View):
+    def get(self, request):
+        return render(request, 'blog/register.html')
+
+    def post(self, request):
+        email = request.POST.get('email').strip()
+        name = request.POST.get('name').strip()
+        password = request.POST.get('password').strip()
+        
+
 
 
 def login(request):
